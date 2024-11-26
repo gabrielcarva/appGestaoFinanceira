3b3e1428fd4a8b5394fefde38268e0ab448c4768
Gabriel Carvalho dos Santos <50636417+gabrielcarva@users.noreply.github.com>
2024-11-25 23:07:38 -0300

    Comit

---
+++ public/footer.php
@@ -0,0 +1,9 @@
+<footer class="bg-light text-center mt-auto">
+        <div class="container p-3">
+            <p>&copy; 2024 Gestão Financeira. Todos os direitos reservados.</p>
+        </div>
+    </footer>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
